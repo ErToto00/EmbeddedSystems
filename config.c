2f109be527b7732fe4fa705cf8e94a6f58c61b2f
Ari <arianna.bardoni@gmail.com>
2026-05-06 15:13:27 +0200

    Add files via upload
    fix2 assignment

--- config.c
+++ config.c
@@ -1,80 +1,51 @@
 #include "config.h"
-#include "timer.h"
-#define CS_ACC LATDbits.LATD6
+#include <xc.h>
+
+// Definizione Chip Select per funzioni interne
+#define CS_ACC LATBbits.LATB3
 
 void input_config(){
-    ANSELA = 0x0000;
-    ANSELB = 0x0000;
-    ANSELC = 0x0000;
-    ANSELD = 0x0000;
-    ANSELE = 0x0000;
-    ANSELG = 0x0000;
+    ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
 }
 
 void uart_config(){
-    TRISDbits.TRISD11 = 1;
-    TRISDbits.TRISD0 = 0;
-
+    TRISDbits.TRISD11 = 1; // RX
+    TRISDbits.TRISD0 = 0;  // TX
     RPINR18bits.U1RXR = 75;
     RPOR0bits.RP64R = 0x01;
-
     U1MODEbits.BRGH = 0;
-    // U1BRG = 468; per baud rate 9600
-    U1BRG = 38 // per baud rate 115200
-
+    U1BRG = 468; 
     U1MODEbits.UARTEN = 1;
     U1STAbits.UTXEN = 1;
-    
-    // Interrupt
-    IPC2bits.U1RXIP = 5;  
-    IFS0bits.U1RXIF = 0;  // Reset flag
-    IEC0bits.U1RXIE = 1;  // Enable interrupt
-
-    U1MODEbits.UARTEN = 1;
-    U1STAbits.UTXEN = 1;
+    IFS0bits.U1RXIF = 0;
+    IEC0bits.U1RXIE = 1;
 }
 
 void spi_config(){
-    TRISAbits.TRISA1 = 1;
-    TRISFbits.TRISF12 = 0;
-    TRISFbits.TRISF13 = 0;
-
-    RPINR20bits.SDI1R = 0b0010001;
-    RPOR12bits.RP109R = 0b000101;
-    RPOR11bits.RP108R = 0b000110;
-
+    TRISAbits.TRISA1 = 1;  // MISO
+    TRISFbits.TRISF12 = 0; // SCK
+    TRISFbits.TRISF13 = 0; // MOSI
+    RPINR20bits.SDI1R = 17;
+    RPOR12bits.RP109R = 5;
+    RPOR11bits.RP108R = 6;
     SPI1CON1bits.MSTEN = 1;
     SPI1CON1bits.MODE16 = 0;
     SPI1CON1bits.CKP = 1;
-
     SPI1CON1bits.PPRE = 3;
     SPI1CON1bits.SPRE = 3;
-
-    SPI1STATbits.SPIROV = 0;
     SPI1STATbits.SPIEN = 1;
 }
 
-#define CS_ACC LATDbits.LATD6
-
 void init_accelerometer() {
-    // Sleep mode
     CS_ACC = 0;
-    spi_write(0x4B); 
-    spi_write(0x01); // Sleep
+    spi_write(0x0F); 
+    spi_write(0x03); 
     CS_ACC = 1;
-    tmr_wait_ms(TIMER1, 3); // 3 ms wait 
-
-    // Active mode
-    CS_ACC = 0;
-    spi_write(0x4C); 
-    spi_write(0x00); // Active
-    CS_ACC = 1;
-    tmr_wait_ms(TIMER1, 10);
 }
 
 void set_bandwidth(int bw_val) {
     CS_ACC = 0;
-    spi_write(0x41);  
+    spi_write(0x10);  
     spi_write(bw_val); 
     CS_ACC = 1;
 }
--- main.c
+++ main.c
@@ -5,25 +5,24 @@
 #include "config.h"
 #include "timer.h"
 
-// Circular buffer for UART1 RX
+// Circular buffer for UART
 #define BUF_SIZE 64
 volatile char buffer[BUF_SIZE];
 volatile int head = 0;
 volatile int tail = 0;
 
-// Chip Select for Accelerometer (RD6 from your exercises)
-#define CS_ACC LATDbits.LATD6
+// Chipselect for accelerometer
+#define CS_ACC LATBbits.LATB3 
 
-// Global variables for the assignment tasks
-int freq = 10;      // Transmission frequency (yy Hz)
-int bandwidth = 15; // Filter bandwidth (xx BW)
+int freq = 10;      // Transmission frequency
+int bandwidth = 15; // Filter bandwidth 
 int ax, ay, az;     // Accelerometer data
 
 void algorithm() {
     tmr_wait_ms(TIMER2, 7);
 }
 
-// UART1 RX Interrupt - Stores characters in a circular buffer
+// UART1 RX Interrupt
 void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void) {
     if (U1STAbits.OERR) U1STAbits.OERR = 0;
     while (U1STAbits.URXDA) {
@@ -37,7 +36,6 @@ void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void) {
     IFS0bits.U1RXIF = 0;
 }
 
-// SPI Write/Read function (from your exercises)
 unsigned int spi_write(unsigned int data) {
     while (SPI1STATbits.SPITBF == 1);
     SPI1BUF = data;
@@ -45,29 +43,35 @@ unsigned int spi_write(unsigned int data) {
     return SPI1BUF;
 }
 
-// Simple function to send a string via UART
 void send_string(char* str) {
     for (int i = 0; str[i] != '\0'; i++) {
-        while (U1STAbits.UTXBF); // Wait for TX buffer to be empty
+        while (U1STAbits.UTXBF);
         U1TXREG = str[i];
     }
 }
 
 int main(void) {
-    // Hardware configurations
     input_config();
     uart_config();
     spi_config();
     
-    TRISGbits.TRISG9 = 0;   // Led2 output 
-    TRISDbits.TRISD6 = 0;   // Chip select    
-    CS_ACC = 1; // Inactive high
+    // Set all CS pins to output
+    TRISDbits.TRISD6 = 0;   // CS3 (MAG)
+    TRISBbits.TRISB4 = 0;   // CS2 (GYRO)
+    TRISBbits.TRISB3 = 0;   // CS1 (ACC)
+    
+    // Deactivate all CS (set to 1)
+    LATDbits.LATD6 = 1; 
+    LATBbits.LATB4 = 1;
+    LATBbits.LATB3 = 1;
 
+    TRISGbits.TRISG9 = 0;   // Led2 output 
+    
     // Initial accelerometer setup
     init_accelerometer();
-    set_bandwidth(15); // Initial BW of 1000 Hz
+    set_bandwidth(15); // Initial BW 1000 Hz 
     
-    tmr_setup_period(TIMER1, 10); // Main loop period: 10ms (100Hz)
+    tmr_setup_period(TIMER1, 10); 
     
     int blink_counter = 0;
     int main_counter = 0;
@@ -76,84 +80,89 @@ int main(void) {
     char out_buf[60];
 
     while (1) {
-        algorithm();
+        algorithm(); 
 
-        // Read UART 
+        // UART parsing 
         while (head != tail) {
             char c = buffer[tail];
             tail = (tail + 1) % BUF_SIZE;
             
-            if (c == '$') msg_idx = 0;
+            if (c == '$') 
+                msg_idx = 0;
+            
             msg[msg_idx++] = c;
             
             if (c == '*') {
                 msg[msg_idx] = '\0';
-                // Handle $HZ,yy* command
+                
                 if (msg[1] == 'H' && msg[2] == 'Z') {
                     int val = atoi(&msg[4]);
-                    if (val == 0 || val == 1 || val == 2 || val == 5 || val == 10) freq = val;
-                    else send_string("$ERR,1*\r\n");
+                    
+                    if (val == 0 || val == 1 || val == 2 || val == 5 || val == 10) 
+                        freq = val; 
+                    
+                    else 
+                        send_string("$ERR,1*\r\n");
                 } 
-                // Handle $BW,xx* command
                 else if (msg[1] == 'B' && msg[2] == 'W') {
                     int val = atoi(&msg[4]);
-                    if (val >= 8 && val <= 15) {
+                    if (val >= 8 && val <= 15) { 
                         bandwidth = val;
                         set_bandwidth(bandwidth);
-                    } else send_string("$ERR,1*\r\n");
+                    } else 
+                        send_string("$ERR,1*\r\n");
                 }
                 msg_idx = 0;
             }
         }
 
-        // Accelerometer aquisition 
-        // Runs every 2 loops (2 * 10ms = 20ms)
+        // Accelerometer 
         if (main_counter % 2 == 0) {
             unsigned int lsb, msb;
             CS_ACC = 0;
             
-            spi_write(0x02 | 0x80); // Read starting from X_LSB (0x02)
-            lsb = spi_write(0x00); msb = spi_write(0x00); // X axis
+            spi_write(0x02 | 0x80); // Start from register 0x02 (ACC_X_LSB)
+            lsb = spi_write(0x00); msb = spi_write(0x00);
+            
             ax = (int)((msb << 8) | (lsb & 0xF8)) >> 3;
+            lsb = spi_write(0x00); msb = spi_write(0x00);
             
-            lsb = spi_write(0x00); msb = spi_write(0x00); // Y axis
             ay = (int)((msb << 8) | (lsb & 0xF8)) >> 3;
+            lsb = spi_write(0x00); msb = spi_write(0x00);
             
-            lsb = spi_write(0x00); msb = spi_write(0x00); // Z axis
             az = (int)((msb << 8) | (lsb & 0xF8)) >> 3;
-            
             CS_ACC = 1;
         }
 
-        // Send acc data 
-        // Calculates the required interval based on freq variable
+        // Send data 
         if (freq > 0 && (main_counter % (100 / freq) == 0)) {
             sprintf(out_buf, "$ACC,%d,%d,%d*\r\n", ax, ay, az);
             send_string(out_buf);
         }
 
-        // Compute and send angles 
-        // Runs every 20 loops (20 * 10ms = 200ms)
+        // Send angles
         if (main_counter % 20 == 0) {
+            
             double roll = atan2(ay, az) * 57.2958;
             double pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 57.2958;
+            
             sprintf(out_buf, "$ANG,%.1f,%.1f*\r\n", roll, pitch);
             send_string(out_buf);
         }
 
         // Blink led2
-        // No busy-waiting: toggles every 500ms (50 cycles)
         blink_counter++;
         if (blink_counter >= 50) {
             LATGbits.LATG9 = !LATGbits.LATG9;
             blink_counter = 0;
         }
 
-        // Counter management to synchronize frequencies
         main_counter++;
         if (main_counter >= 100) main_counter = 0;
         
-        // Wait for the end of the 10ms period
-        tmr_wait_period(TIMER1);
+        int ret = tmr_wait_period(TIMER1); 
+        if(ret == 1){
+            send_string("!!! missed deadline !!!\r\n");
+        }
     }
 }
